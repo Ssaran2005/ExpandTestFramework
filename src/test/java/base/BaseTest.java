356db01d19b347cee1b80a143b1e5f160565d905
Ssaran2005 <saransakthivel2005@gmail.com>
2026-05-07 11:19:28 +0530

    Initial Selenium framework setup

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,31 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import utils.ConfigReader;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    ConfigReader configReader = new ConfigReader();
+
+    @BeforeMethod
+    public void setup() {
+
+        driver = DriverFactory.initializeDriver();
+
+        driver.get(configReader.getBaseUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+
+            driver.quit();
+        }
+    }
+}
